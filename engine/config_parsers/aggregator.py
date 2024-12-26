357c41a42058f43a2dd023c961d26ead990c0c94
hugo <hugo@baudchon.com>
2024-12-26 16:12:56 -0500

    Removed unused polygon_type param from Aggregator config

--- engine/config_parsers/aggregator.py
+++ engine/config_parsers/aggregator.py
@@ -5,7 +5,6 @@ class AggregatorConfig(BaseConfig):
     score_threshold: float = 0.2
     nms_threshold: float = 0.5
     nms_algorithm: str = 'iou'
-    polygon_type: str = 'box'
     detector_score_weight: float = 0.5
     segmenter_score_weight: float = 0.5
     scores_weighting_method: str = 'weighted_geometric_mean'
