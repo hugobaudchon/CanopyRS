ca77c424b6660d99c6a696faaca6a5a9eecb1662
Hugo <hugo@baudchon.com>
2025-01-22 15:18:22 -0500

    Removed oam-tcd from model registry for now

--- engine/models/registry.py
+++ engine/models/registry.py
@@ -1,6 +1,6 @@
 from engine.models.detector.faster_rcnn import FasterRCNNWrapper
 from engine.models.detector.retina_net import RetinaNetWrapper
-from engine.models.segmenter.oam_tcd_torchscript import OamTcdTorchScriptWrapper
+# from engine.models.segmenter.oam_tcd_torchscript import OamTcdTorchScriptWrapper
 from engine.models.segmenter.sam import SamPredictorWrapper
 from engine.models.segmenter.sam2 import Sam2PredictorWrapper
 
@@ -14,7 +14,7 @@ DETECTOR_REGISTRY = {
 SEGMENTER_REGISTRY = {
     'sam': SamPredictorWrapper,
     'sam2': Sam2PredictorWrapper,
-    'oam-tcd': OamTcdTorchScriptWrapper
+    # 'oam-tcd': OamTcdTorchScriptWrapper
 }
 
 
