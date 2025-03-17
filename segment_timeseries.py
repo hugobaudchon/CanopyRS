622a5e1d830828b32ed9ead48b881fae4ec15369
Berend Brandt <berend.brandt@gmail.com>
2025-03-17 15:11:54 -0400

    Working example, 6 frames, 12 initial bboxes

--- segment_timeseries.py
+++ segment_timeseries.py
@@ -5,7 +5,7 @@ import torch
 import matplotlib.pyplot as plt
 from PIL import Image
 import geopandas as gp
-
+import re
 from sam2.sam2_video_predictor import SAM2VideoPredictor
 import rasterio
 import psutil
@@ -14,17 +14,6 @@ def show_box(box, ax):
     x0, y0 = box[0], box[1]
     w, h = box[2] - box[0], box[3] - box[1]
     ax.add_patch(plt.Rectangle((x0, y0), w, h, edgecolor='green', facecolor=(0, 0, 0, 0), lw=2))
-
-# def show_mask(mask, ax, obj_id=None, random_color=False):
-#     if random_color:
-#         color = np.concatenate([np.random.random(3), np.array([0.6])], axis=0)
-#     else:
-#         cmap = plt.get_cmap("tab10")
-#         cmap_idx = 0 if obj_id is None else obj_id
-#         color = np.array([*cmap(cmap_idx)[:3], 0.6])
-#     h, w = mask.shape[-2:]
-#     mask_image = mask.reshape(h, w, 1) * color.reshape(1, 1, -1)
-#     ax.imshow(mask_image)
 
 def show_mask(mask, ax, obj_id=None, random_color=False):
     if random_color:
@@ -37,7 +26,14 @@ def show_mask(mask, ax, obj_id=None, random_color=False):
     mask_image = mask.reshape(h, w, 1) * color.reshape(1, 1, -1)
     ax.imshow(mask_image)
 
-def timeseries_sam2(input_folder:str, output_folder:str, bbox_file:str):
+
+def gis_bbox_to_pixel(bbox, src):
+    max_row, min_col = src.index(bbox[0], bbox[1])
+    min_row, max_col = src.index(bbox[2], bbox[3])
+
+    return min_col, min_row, max_col, max_row
+
+def timeseries_sam2(input_folder:str, output_folder:str, bbox_file:str, max_bboxes:int):
     ann_frame_idx=0
     if torch.cuda.is_available():
         device = torch.device("cuda")
@@ -58,19 +54,15 @@ def timeseries_sam2(input_folder:str, output_folder:str, bbox_file:str):
         p for p in os.listdir(input_folder)
         if os.path.splitext(p)[-1] in [".tif"]
     ]
+    # frame_names.sort(key=lambda p: int(re.search(r'\d+', os.path.splitext(p)[0]).group()))
+
     frame_names.sort(key=lambda p: int(os.path.splitext(p)[0]))
     inference_state = predictor.init_state(video_path=input_folder)
     predictor.reset_state(inference_state)
     bboxes_gdf = gp.read_file(bbox_file)
     bboxes = bboxes_gdf["geometry"].apply(lambda geom: geom.bounds)  # (minx, miny, maxx, maxy)
-    bboxes = bboxes[:10] # for testing, only take x bboxes for now
+    bboxes = bboxes[:max_bboxes] # for testing, only take x bboxes for now
     bboxes.reset_index()
-
-    def gis_bbox_to_pixel(bbox, src):
-        max_row, min_col = src.index(bbox[0], bbox[1])
-        min_row, max_col = src.index(bbox[2], bbox[3])
-
-        return min_col, min_row, max_col, max_row
 
     with rasterio.open(input_folder + '/' + frame_names[0]) as src:
         bboxes = bboxes.apply(gis_bbox_to_pixel, args=(src,))
@@ -116,10 +108,12 @@ def timeseries_sam2(input_folder:str, output_folder:str, bbox_file:str):
     #     obj_id=300,
     #     box=box,
     # )
-
+    plt.savefig(os.path.join(output_folder, f"frame_{ann_frame_idx:04d}_bboxes.png"), bbox_inches="tight", dpi=300)
     plt.figure(figsize=(9, 6))
     plt.title(f"frame {ann_frame_idx}")
     plt.imshow(Image.open(os.path.join(input_folder, frame_names[ann_frame_idx])))
+    # plt.savefig(os.path.join(output_folder, f"frame_{ann_frame_idx:04d}_bboxes.png"), bbox_inches="tight", dpi=300)
+    # plt.show()
 
     video_segments = {}  # video_segments contains the per-frame segmentation results
     for out_frame_idx, out_obj_ids, out_mask_logits in predictor.propagate_in_video(inference_state):
@@ -128,44 +122,19 @@ def timeseries_sam2(input_folder:str, output_folder:str, bbox_file:str):
             for i, out_obj_id in enumerate(out_obj_ids)
         }
 
-
-
-    # render the segmentation results every few frames
-    # vis_frame_stride = 1
-    # plt.close("all")
-    # for out_frame_idx in range(0, len(frame_names), vis_frame_stride):
-    #     fig, ax = plt.subplots(figsize=(6, 4))  # Use `fig` and `ax` for saving
-    #     ax.set_title(f"frame {out_frame_idx}")
-    #
-    #     # Load and display the original frame
-    #     frame_path = os.path.join(input_folder, frame_names[out_frame_idx])
-    #     frame_img = Image.open(frame_path)
-    #     ax.imshow(frame_img)
-    #
-    #     # Overlay all masks
-    #     for out_obj_id, out_mask in video_segments[out_frame_idx].items():
-    #         show_mask(out_mask, ax, obj_id=out_obj_id)  # Ensure masks are added to `ax`
-    #
-    #     # Save the frame with the overlaid masks
-    #     save_path = os.path.join(output_folder, f"frame_{out_frame_idx:04d}.png")
-    #     plt.savefig(save_path, bbox_inches="tight", dpi=300)
-    #     plt.close(fig)  # Close the figure to free memory
-    #
-    #     print(f"Saved {save_path}")
-
-    vis_frame_stride = 1
     plt.close("all")
-    for out_frame_idx in range(0, len(frame_names), vis_frame_stride):
-        plt.figure(figsize=(6, 4))
+    for out_frame_idx in range(0, len(frame_names), 1):
+        plt.figure(figsize=(9, 6))
         plt.title(f"frame {out_frame_idx}")
         plt.imshow(Image.open(os.path.join(input_folder, frame_names[out_frame_idx])))
         for out_obj_id, out_mask in video_segments[out_frame_idx].items():
             show_mask(out_mask, plt.gca(), obj_id=out_obj_id)
-        # plt.show()
         plt.savefig(os.path.join(output_folder, f"frame_{out_frame_idx:04d}.png"), bbox_inches="tight", dpi=300)
+
 if __name__=="__main__":
     timeseries_sam2(
         '../montreal_forest_data/nice_cut/tiny',
-        '../montreal_forest_data/nice_cut/output',
-        '../montreal_forest_data/nice_cut/0_formain_gr0p05_infer.gpkg'
+        '../montreal_forest_data/nice_cut/output/tiny_warped',
+        '../montreal_forest_data/nice_cut/05_28_0_gr0p05_infer.gpkg',
+        max_bboxes=12
     )
